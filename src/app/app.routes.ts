72134d80452223778ee0107ecb86561dbf8c5820
ppilarodriguez <pilar.rodriguez2430@gmail.com>
2025-10-10 15:00:33 -0300

    Primer commit <3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { WelcomePage } from './pages/welcome-page/welcome-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'' , component:WelcomePage},
+    {path:'home', component:Home},
+    {path:'login', component:Login},
+    {path:'register', component:Register},
+    {path:'**',redirectTo: ''}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
