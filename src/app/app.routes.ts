be0979633336aa7ed9e7f4289d3dfaec1cf2d815
ppilarodriguez <pilar.rodriguez2430@gmail.com>
2025-10-11 17:26:21 -0300

    Agrego vista de recuperar contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { WelcomePage } from './pages/welcome-page/welcome-page';
 import { ProductoDetalle} from './components/producto-detalle/producto-detalle';
+import { RecoverPassword } from './pages/recover-password/recover-password';
 
 export const routes: Routes = [
     {path:'' , component:WelcomePage},
     {path:'home', component:Home},
     {path:'login', component:Login},
     {path:'register', component:Register},
-    { path: 'producto/:id', component: ProductoDetalle},
-    {path:'**',redirectTo: ''},
-
+    {path: 'producto/:id', component: ProductoDetalle},
+    {path:'recoverPassword',component:RecoverPassword},
+    {path:'**',redirectTo: ''}
 ];
