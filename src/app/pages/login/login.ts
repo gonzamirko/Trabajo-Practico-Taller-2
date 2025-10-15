be0c5433af2f65d759ccdaf299c7cf676871edff
MateoColace <166334360+MateoColace@users.noreply.github.com>
2025-10-14 22:00:41 -0300

    Agregue la clase Usuario, ahora se puede registrar usuario e ingresar al home con email y contraseña

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { RecoverPassword } from '../recover-password/recover-password';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule,RecoverPassword],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  email: string = '';
+  password: string = '';
+
+  constructor(private router : Router){}
+  
+ingresarAHome(){
+  const usuariosGuardados = JSON.parse(localStorage.getItem('usuarios') || '[]');
+  const usuarioEncontrado = usuariosGuardados.find((u: any) => 
+    u.email === this.email && u.password === this.password);
+
+  if(usuarioEncontrado){
+    localStorage.setItem('usuarioLogueado', JSON.stringify(usuarioEncontrado));
+    this.router.navigate(['/home']);
+  }else{
+    alert('Usuario o contraseña incorrecta');
+  }
+  
+}
 
 }
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Usuario } from '../../components/usuario/usuario';
+import { Router } from '@angular/router';
 
+ 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+
+  constructor(private router : Router){}
+
+  nombre: string = '';
+  apellido: string = '';
+  email: string = '';
+  password: string = '';
+  direccion: string = '';
+
+  usuario = new Usuario();
+  usuariosGuardados = JSON.parse(localStorage.getItem('usuarios') || '[]');
+
+  registrarCliente(){
+
+    const yaExiste = this.usuariosGuardados.some((u: Usuario) => u.email === this.email);
+
+    if(yaExiste){
+      alert('Usuario existente')
+    }else{
+      
+    this.usuario.nombre = this.nombre;
+    this.usuario.apellido = this.apellido;
+    this.usuario.email = this.email;
+    this.usuario.password = this.password;
+    this.usuario.direccion = this.direccion;
+    this.usuariosGuardados.push(this.usuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(this.usuariosGuardados));
+    this.router.navigate(['/login']);
+    }
+  }
+
 
 }
