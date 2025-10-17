765f11af17facccf31de7988280f878db191458b
angela quelca <anahi.laurani@gmail.com>
2025-10-17 20:44:11 -0300

    primeros cambios

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RecoverPassword } from '../recover-password/recover-password';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule,RecoverPassword],
+  imports: [RouterModule,RecoverPassword,ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
 
+
+export class Login {
+ 
 }
+
