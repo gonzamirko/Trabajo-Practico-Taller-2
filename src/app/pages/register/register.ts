88b0d64169501c998a817a2f582512e471959126
MeliYesabel <melinayesabel@gmail.com>
2025-10-17 23:02:57 -0300

    se modifico el register ts para trabajar con FormsControl y FormsGroup, se agregaron las validaciones de campos y logica de condicion contraseña, todavia hay errores

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,47 +1,73 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { FormsModule,
+  FormControl,
+  FormGroup,
+  Validator,
+  ReactiveFormsModule,
+  Validators
+ } from '@angular/forms';
 import { Usuario } from '../../components/usuario/usuario';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
 
  
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
-  templateUrl: './register.html',
-  styleUrl: './register.css'
+  imports: [FormsModule, RouterOutlet, ReactiveFormsModule, JsonPipe],
+  templateUrl: '../register/register.html',
+  styleUrl: '../register/register.css'
 })
 export class Register {
 
-  constructor(private router : Router){}
+ 
+  constructor(private router : Router){}  
 
-  nombre: string = '';
-  apellido: string = '';
-  email: string = '';
-  password: string = '';
-  direccion: string = '';
+ // Validamos que todos los parametros este completos 
+
+  form = signal<FormGroup>(
+    new FormGroup({
+  nombre: new FormControl('', Validators.required),
+  apellido: new FormControl('', Validators.required),
+  email: new FormControl('',  [Validators.required, Validators.email]),
+  password:new FormControl('', Validators.required),
+  direccion: new FormControl('', Validators.required),
+  })
+)
+
+formValue = this.form().value;
 
   usuario = new Usuario();
   usuariosGuardados = JSON.parse(localStorage.getItem('usuarios') || '[]');
 
-  registrarCliente(){
+ registrarCliente() {
+  const yaExiste = this.usuariosGuardados.some((u: Usuario) => u.email === this.formValue.email);
 
-    const yaExiste = this.usuariosGuardados.some((u: Usuario) => u.email === this.email);
+  // CONSIGNA: Se requiere implementar validación de la complejidad de la contraseña proporcionada. No será posible utilizar el mismo mail para registrar múltiples usuarios.
 
-    if(yaExiste){
-      alert('Usuario existente')
-    }else{
-      
-    this.usuario.nombre = this.nombre;
-    this.usuario.apellido = this.apellido;
-    this.usuario.email = this.email;
-    this.usuario.password = this.password;
-    this.usuario.direccion = this.direccion;
-    this.usuariosGuardados.push(this.usuario);
-
-    localStorage.setItem('usuarios', JSON.stringify(this.usuariosGuardados));
-    this.router.navigate(['/login']);
-    }
+  // condicion validación de la complejidad de la contraseña proporcionada 
+  const condicionContra = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+  if (!condicionContra.test(this.form().get('password')?.value)) {
+    alert('La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un símbolo');
+    return;
+  }
+ // condicion de mail duplicados
+    if (yaExiste) {
+    alert('Ya existe un usuario con este email');
+    return;
   }
 
+
+  this.usuario.nombre = this.formValue.nombre;
+  this.usuario.apellido = this.formValue.apellido;
+  this.usuario.email = this.formValue.email;
+  this.usuario.password = this.formValue.password;
+  this.usuario.direccion = this.formValue.direccion;
+
+  this.usuariosGuardados.push(this.usuario);
+  localStorage.setItem('usuarios', JSON.stringify(this.usuariosGuardados));
+
+  alert('Usuario registrado con éxito');
+  this.router.navigate(['/login']);
+}
 
 }
