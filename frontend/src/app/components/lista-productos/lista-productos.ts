a9acd02bd85bd3443f9d76fe94ad1cc0753611a4
Gonzalo Greatti <gonza_lauty@outlook.com>
2025-11-11 22:53:41 -0300

    hago que los filtros se guarden cuando cambias de pantalla

--- frontend/src/app/components/lista-productos/lista-productos.ts
+++ frontend/src/app/components/lista-productos/lista-productos.ts
@@ -1,57 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProducto } from './i-producto';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router, ActivatedRoute } from '@angular/router';
 import { ProductoServicio } from '../../api/services/productos/producto.service';
 import { Observable } from 'rxjs'; 
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-lista-productos',
-  templateUrl: './lista-productos.html',
-  styleUrls: ['./lista-productos.css'],
-  standalone: true,
-  imports: [CommonModule, RouterLink,FormsModule]
+  selector: 'app-lista-productos',
+  templateUrl: './lista-productos.html',
+  styleUrls: ['./lista-productos.css'],
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormsModule]
 })
 export class ListaProductos implements OnInit {
 
+  productos$: Observable<IProducto[]> | undefined;
 
-  productos$: Observable<IProducto[]> | undefined;
+  nombre: string = "";
+  clasificacion: string = "";
+  precioMin?: number;
+  precioMax?: number;
 
-  constructor(private productoServicio: ProductoServicio) {}
+  constructor(
+    private productoServicio: ProductoServicio,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  /*ngOnInit(): void {
-   
-    this.productos$ = this.productoServicio.obtenerProductos();
-   
+  ngOnInit(): void {
+   
+    this.route.queryParams.subscribe(params => {
+      this.nombre = params['nombre'] || "";
+      this.clasificacion = params['clasificacion'] || "";
+      this.precioMin = params['precioMin'] ? Number(params['precioMin']) : undefined;
+      this.precioMax = params['precioMax'] ? Number(params['precioMax']) : undefined;
 
-  }*/
-nombre: string = "";
-clasificacion: string = "";
-precioMin?: number;
-precioMax?: number;
+      this.cargarProductos(); 
+    });
+  }
 
-ngOnInit(): void {
-  this.cargarProductos();
-}
+  cargarProductos() {
+    const filtros: any = {};
 
-cargarProductos() {
-  this.productos$ = this.productoServicio.obtenerProductos();
-}
+    if (this.nombre) filtros.nombre = this.nombre;
+    if (this.clasificacion) filtros.clasificacion = this.clasificacion;
+    if (this.precioMin) filtros.precioMin = this.precioMin;
+    if (this.precioMax) filtros.precioMax = this.precioMax;
 
-filtrar() {
-  const filtros: any = {};
+    this.productos$ = this.productoServicio.obtenerProductos(filtros);
+  }
 
-  if (this.nombre) filtros.nombre = this.nombre;
-  if (this.clasificacion) filtros.clasificacion = this.clasificacion;
-  if (this.precioMin) filtros.precioMin = this.precioMin;
-  if (this.precioMax) filtros.precioMax = this.precioMax;
-
-  this.productos$ = this.productoServicio.obtenerProductos(filtros);
-}
-
-
-
-
+  filtrar() {
+  
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        nombre: this.nombre || null,
+        clasificacion: this.clasificacion || null,
+        precioMin: this.precioMin || null,
+        precioMax: this.precioMax || null
+      },
+      queryParamsHandling: 'merge'
+    });
+  }
 
 }
